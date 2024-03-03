482db2993076ade99a1f3945a75b14433d8ae3e5
efe <113731512+efenatuyo@users.noreply.github.com>
2024-03-03 12:50:14 +0100

    Update users.py

--- robloxGroup/listeners/users.py
+++ robloxGroup/listeners/users.py
@@ -1,18 +1,19 @@
 import aiohttp, asyncio
-from typing import Type
+from typing import Type, Union
 from ..cookie import RobloxCookie
 from ..helpers import DotDict
-
+from ..functions import role
 class watch:
     users: list = []
     checked_once = False
-    def __init__(self, cookie: Type[RobloxCookie], group_id: int, group: Type[object]) -> None:
-        self.cookie, self.group_id, self.group = cookie, group_id, group
+    def __init__(self, cookie: Type[RobloxCookie], group: Type[object]) -> None:
+        self.cookie, self.group = cookie, group
         
     async def start(self) -> None:
+        on_join_role = await role.get(self.group)["roles"][1]["id"]
         async with aiohttp.ClientSession() as session:
             while True:
-                async with session.get(f"https://groups.roblox.com/v1/groups/{self.group_id}/roles/80273647/users?cursor=&limit=100&sortOrder=Desc", cookies={".ROBLOSECURITY": self.cookie.cookie}, headers={"x-csrf-token": await self.cookie.x_token(session)}) as response:
+                async with session.get(f"https://groups.roblox.com/v1/groups/{self.group.group_id}/roles/{on_join_role}/users?cursor=&limit=100&sortOrder=Desc", cookies={".ROBLOSECURITY": self.cookie.cookie}, headers={"x-csrf-token": await self.cookie.x_token(session)}) as response:
                     if response.status == 200:
                         for user in (await response.json())["data"]:
                             if not user["userId"] in self.users:

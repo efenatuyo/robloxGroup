ee772ff65d5e642076c6c787793f0d9211d12b9c
efe <113731512+efenatuyo@users.noreply.github.com>
2024-03-03 12:56:45 +0100

    Update role.py

--- robloxGroup/functions/role.py
+++ robloxGroup/functions/role.py
@@ -1,20 +1,19 @@
 import aiohttp
 from typing import Type, Union
 from ..helpers import DotDict
-from ..commands import Bot
 from ..helpers import DotDict
 
-async def change(group: Type[Bot], user_id: Union[int, str], role_id: Union[int, str]) -> Type[DotDict]:
+async def change(group: Type[object], user_id: Union[int, str], role_id: Union[int, str]) -> Type[DotDict]:
     async with aiohttp.ClientSession() as session:
         async with session.patch(f"https://groups.roblox.com/v1/groups/{group.group_id}/users/{user_id}", cookies={".ROBLOSECURITY": group.cookie.cookie}, json={"roleId": role_id}, headers={"x-csrf-token": await group.cookie.x_token(session)}) as response:
             return DotDict(await response.json())
 
-async def guilds_user(group: Type[Bot], user_id: Union[int, str]) -> Type[DotDict]:
+async def guilds_user(group: Type[object], user_id: Union[int, str]) -> Type[DotDict]:
     async with aiohttp.ClientSession() as session:
         async with session.get(f"https://groups.roblox.com/v1/users/{user_id}/groups/roles?includeLocked=false", cookies={".ROBLOSECURITY": group.cookie.cookie}, headers={"x-csrf-token": await group.cookie.x_token(session)}) as response:
             return DotDict(await response.json())
 
-async def get(group: Type[Bot]) -> Type[DotDict]:
+async def get(group: Type[object]) -> Type[DotDict]:
     async with aiohttp.ClientSession() as session:
         async with session.get(f"https://groups.roblox.com/v1/groups/{group.group_id}/roles", cookies={".ROBLOSECURITY": group.cookie.cookie}, headers={"x-csrf-token": await group.cookie.x_token(session)}) as response:
             return DotDict(await response.json())

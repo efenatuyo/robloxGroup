f9fe1c02f28515477beea82d6dcc02e512dfcdf6
efe <113731512+efenatuyo@users.noreply.github.com>
2024-03-03 12:53:37 +0100

    Update role.py

--- robloxGroup/functions/role.py
+++ robloxGroup/functions/role.py
@@ -13,3 +13,8 @@ async def guilds_user(group: Type[Bot], user_id: Union[int, str]) -> Type[DotDic
     async with aiohttp.ClientSession() as session:
         async with session.get(f"https://groups.roblox.com/v1/users/{user_id}/groups/roles?includeLocked=false", cookies={".ROBLOSECURITY": group.cookie.cookie}, headers={"x-csrf-token": await group.cookie.x_token(session)}) as response:
             return DotDict(await response.json())
+
+async def get(group: Type[Bot]) -> Type[DotDict]:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"https://groups.roblox.com/v1/groups/{group.group_id}/roles", cookies={".ROBLOSECURITY": group.cookie.cookie}, headers={"x-csrf-token": await group.cookie.x_token(session)}) as response:
+            return DotDict(await response.json())
